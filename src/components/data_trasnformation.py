36f7a05984311acf8b6e41c2b10535a28d0c6c60
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2024-01-29 23:53:18 +0530

    data transformation completed

--- src/components/data_trasnformation.py
+++ src/components/data_trasnformation.py
@@ -1,8 +1,9 @@
 from src.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact
+from sklearn.model_selection import train_test_split
 from src.entity.config_entity import DataTransformationConfig
 from src.exception import ham_spam
 from src.logger import logging
-from src.constant.constants import TARGET_NAME,FEATURES_NAME
+from src.constant.constants import *
 import os
 import sys
 import pandas as pd
@@ -14,8 +15,14 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from collections import Counter
+from nltk.stem import SnowballStemmer
+
 
 # nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
 
 class DataTransformation:
     def __init__(self, data_transformation_config: DataTransformationConfig,
@@ -58,9 +65,9 @@ class DataTransformation:
 
         stop_words = set(stopwords.words('english'))
         
-        lemmatizer = WordNetLemmatizer()
+        stemmer = SnowballStemmer("english")
         
-        words = [lemmatizer.lemmatize(word) for word in word_tokenize(text) if word.lower() not in stop_words]
+        words = [stemmer.stem(word) for word in word_tokenize(text) if word.lower() not in stop_words]
         
         return ' '.join(words)
 
@@ -74,8 +81,8 @@ class DataTransformation:
         df[column] = df[column].apply(self._preprocess_text)
 
         return df
-    
-    ## this method we put inside the utils fuction because we are use this method multiple times
+   
+
     def _save_transformed_data(self, df: pd.DataFrame, save_path: str) -> None:
         df.to_csv(save_path, index=False)
         logging.info(f"Saving the Transformed data to: {save_path}")
@@ -88,24 +95,38 @@ class DataTransformation:
 
         # df = self._read_data(self.data_validation_artifact.validated_data_path)
         df = self._read_data(self.data_validation_artifact)
-        
         df = self._label_encode_target(df)
-        df = self._remove_duplicates(df)
-       
-        df = self._preprocess_text_column(df, 'Text_Message')
         logging.info(f"Data :\n {df.head()}")
-        os.makedirs(self.config.data_transformation_dir, exist_ok=True)
-        transformed_data_path = os.path.join(self.config.data_transformation_dir,
-                                            self.config.data_transformation_file_name)
-        self._save_transformed_data(df, transformed_data_path)
+        df = self._remove_duplicates(df)
 
+        df = self._preprocess_text_column(df, FEATURES_NAME)
+        data = df.dropna(subset=[FEATURES_NAME])
+
+        os.makedirs(self.config.transform_dir_path, exist_ok=True)
+        transformed_data_path = os.path.join(self.config.transform_dir_path,
+                                            self.config.trasnform_data_file_name)
+        self._save_transformed_data(data, transformed_data_path)
+
+  
+        train_data,test_data = train_test_split(data, test_size=TEST_SET_SIZE, random_state=RANDOM_STATE)
+
+        train_test_file_dir = self.config.train_test_file_path
+        os.makedirs(train_test_file_dir, exist_ok= True)
+
+        train_file_path = os.path.join(train_test_file_dir,TRAIN_FILE_NAME)
+        test_file_path = os.path.join(train_test_file_dir,TEST_FILE_NAME)
+
+        self._save_transformed_data(train_data,train_file_path)
+        self._save_transformed_data(test_data,test_file_path)
 
         logging.info(f"{30*'===='}")
         logging.info(f"{10*'=='}Data Transformation Completed...{10*'=='}")
         logging.info(f"{30*'===='}")
 
         data_transformation_artifact = DataTransformationArtifact(
-            data_transformation_path=transformed_data_path
+            data_transformation_path=transformed_data_path,
+            train_file_path = train_file_path,
+            test_file_path = test_file_path,
         )
 
         return data_transformation_artifact

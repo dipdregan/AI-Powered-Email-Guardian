d843907c83ae9f8c2530ce90f94bb97273de5860
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2024-01-30 00:17:05 +0530

    training testing done

--- testing.py
+++ testing.py
@@ -1,23 +1,40 @@
 from components.data_ingetion import DataIngestion
-from entity.config_entity import DataIngestionConfig,RootConfig
+from entity.config_entity import DataIngestionConfig,RootConfig,DataTransformationConfig
 
 from components.data_validation import DataValidation
-from entity.config_entity import DataValidationConfig
-# from entity.artifact_entity import DataIngestionArtifact
+from entity.config_entity import *
+from components.data_trasnformation import DataTransformation
+from entity.artifact_entity import *
+from src.components.model_trainer import ModelTrainer
+from src.logger import logging
 
-
-config = DataValidationConfig(
+config = ModelTrainerConfig(
     root_dir=RootConfig()
 )
-# print(config.__dict__)
-data_ingestion_artifact = r"F:\End_To_End_project\Ham_Spam_Classifier(ETE)\artifact\01_23_2024_22_49_03\data_ingetion\unzip_data"
-obj = DataValidation(data_validation_config=config,
-                     data_ingestion_artifact=data_ingestion_artifact)
-obj.initiate_data_validation()
+logging.info(config.__dict__)
+artifact_paths = {
+    "train_file_path": "artifact/01_29_2024_19_16_48/data_transformation/train_test_data/train.csv",
+    "test_file_path": "artifact/01_29_2024_19_16_48/data_transformation/train_test_data/test.csv"
+}
+
+data_transformation_artifact = DataTransformationArtifact(**artifact_paths)
+# print(data_transformation_artifact.test_file_path)
+# print(data_transformation_artifact.train_file_path)
+obj = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                   model_trainer_config=config)
+obj.initiate_model_trainer()
 
 
+# config = DataTransformationConfig(
+#     root_dir=RootConfig()
+# )
 
-######
+# # print(config.__dict__)
+# data_ingestion_artifact = r"F:\End_To_End_project\Ham_Spam_Classifier(ETE)\artifact\01_25_2024_15_18_34\data_validation\invalid\invalidated_data.csv"
+# obj = DataTransformation(config,data_ingestion_artifact)
+# obj.initiate_data_transformation()
+
+
 
 
 ##### ============================ Data Ingestion Testing Completed ================================

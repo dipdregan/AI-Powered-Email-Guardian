f04d8b942826317bc6f048dbfe0e8febfa204c79
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2024-02-20 22:28:22 +0530

    mlops started again

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def prediction(message):
     return y_pred_prob, y_pred, message
 
 if __name__ == '__main__':
-    app.run(host='localhost',port =8000, debug=True)
+    # app.run(host='localhost',port =8000, debug=True)
+    app.run(host='0.0.0.0',port =8000,)
